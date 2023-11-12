ef8d628f18a1d5e5453dda7cd6a686e5adb7b719
YuNARinRIn <cylim456@gmail.com>
2023-11-12 21:20:53 +0800

    SlowPoke Moving Car

--- ParkingSim/Assets/Script/CarNavMesh.cs
+++ ParkingSim/Assets/Script/CarNavMesh.cs
@@ -6,30 +6,30 @@ using UnityEngine.AI;
 
 public class CarNavMesh : MonoBehaviour
 {
-    [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private GameObject movePositionTransform;
 
     private NavMeshAgent navMeshAgent;
 
     void Start()
     {
         // Assuming you have already dragged the MainRoad instance into the Unity Editor
-        GameObject mainRoadInstance = GameObject.Find("Main Road"); // Change "MainRoad" to the actual name of your instance
+        GameObject mainRoadInstance = GameObject.Find("Despawn"); // Change "MainRoad" to the actual name of your instance
 
         if (mainRoadInstance != null)
         {
             // Find the DespawnRoad within the MainRoad instance
-            Transform despawnRoadTransform = mainRoadInstance.transform.Find("Despawn Road");
+            //Transform despawnRoadTransform = mainRoadInstance.transform.Find("Despawn Road");
 
-            if (despawnRoadTransform == null)
-            {
+            //if (mainRoadInstance == null)
+            //{
                 // Do something with despawnRoadTransform
                 // For example, set the destination of the nav mesh agent
-                navMeshAgent.destination = despawnRoadTransform.position;
-            }
-            else
-            {
-                Debug.LogError("DespawnRoad not found in MainRoad instance."); 
-            }
+                navMeshAgent.destination = mainRoadInstance.transform.position;
+            //}
+            //else
+            //{
+            //    Debug.LogError("DespawnRoad not found in MainRoad instance."); 
+            //}
         }
         else
         {
